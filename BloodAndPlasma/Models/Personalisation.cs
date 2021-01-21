fa5105213c290896aac7895402946ebf2718b291
Vivian <vivian.roberts@madetech.com>
2021-01-21 14:49:33 +0000

    Removed notify client as it contains blocking call and to reuse HttpClient

---
+++ BloodAndPlasma/Models/Personalisation.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BloodAndPlasma.Models
+{
+    public class Personalisation
+    {
+        [JsonPropertyName("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("donor_id")]
+        public int DonorId { get; set; }
+    }
+}
---
+++ BloodAndPlasma/Models/SendEmail.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BloodAndPlasma.Models
+{
+    public class SendEmail
+    {
+        [JsonPropertyName("email_address")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("template_id")]
+        public string TemplateId { get; set; }
+
+        [JsonPropertyName("personalisation")]
+        public Personalisation Personalisation { get; set; }
+
+        public static SendEmail Create(UserData userData, string templateId)
+        {
+            return new SendEmail
+            {
+                Email = userData.Email,
+                TemplateId = templateId,
+                Personalisation = new Personalisation
+                {
+                    FirstName = userData.FirstName,
+                    DonorId = userData.DonorId
+                }
+            };
+        }
+    }
+}
